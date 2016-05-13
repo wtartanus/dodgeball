73840bfa9c342d5eb9cd53d1e3f26c5ca0a5af72
Wojtek Tartanus <wtartanus@gmail.com>
2016-05-13 14:12:07 +0100

    Finished match class

--- console.rb
+++ console.rb
@@ -2,9 +2,10 @@ require( 'pg' )
 require( 'pry-byebug' )
 require_relative( 'db/sql_runner.rb' )
 require_relative( 'models/team.rb' )
-require_relative( 'models/team.rb' )
+require_relative( 'models/match.rb' )
 
 Team.delete_all()
+Match.delete_all()
 
 team1 = Team.new( { 'name' => 'Wizards'} )
 team2 = Team.new( { 'name' => 'Heat'} )
@@ -17,6 +18,20 @@ t2 = team2.save()
 t3 = team3.save()
 t4 = team4.save()
 t5 = team5.save()
+
+match1 = Match.new( { 'home_team_id' => t1.id, 'away_team_id' => t2.id, 'home_team_score' => 0, 
+                      'away_team_score' => 3 } )
+match2 = Match.new( { 'home_team_id' => t3.id, 'away_team_id' => t4.id, 'home_team_score' => 2, 
+                      'away_team_score' => 1 } )
+match3 = Match.new( { 'home_team_id' => t5.id, 'away_team_id' => t1.id, 'home_team_score' => 0, 
+                      'away_team_score' => 4 } )
+match4 = Match.new( { 'home_team_id' => t2.id, 'away_team_id' => t1.id, 'home_team_score' => 1, 
+                      'away_team_score' => 3 } )
+
+m1 = match1.save()
+m2 = match2.save()
+m3 = match3.save()
+m4 = match4.save()
 
 binding.pry
 nil
--- models/match.rb
+++ models/match.rb
@@ -3,4 +3,54 @@
 ###Should do: play game, list all match, list all match according to team
 
 class Match
+
+
+  attr_accessor :id, :home_team_id, :away_team_id, :home_team_score, 
+                :away_team_score
+
+
+   def initialize( options )
+    @id = options[ 'id' ].to_i
+    @home_team_id = options[ 'home_team_id' ].to_i
+    @away_team_id = options[ 'away_team_id' ].to_i
+    @home_team_score = options[ 'home_team_score' ].to_i
+    @away_team_score = options[ 'away_team_score' ].to_i 
+   end
+
+   def save()
+     sql = "INSERT INTO 
+     match(home_team_id, away_team_id, home_team_score, away_team_score) VALUES(#{@home_team_id}, #{@away_team_id}, #{@home_team_score}, 
+       #{@away_team_score}) RETURNING *"
+     match = Match.map_item( sql )
+
+     return match
+   end
+
+   def self.all()
+     sql = "SELECT * FROM match"
+     match = Match.map_items( sql )
+
+     return match
+   end
+
+
+   def self.map_items( sql )
+     games = SqlRunner.run( sql )
+     result = games.map { |match| Match.new( match )}
+
+     return result
+   end
+
+   def self.map_item( sql )
+     match = Match.map_items( sql )
+     match = match.first
+
+     return match
+   end
+
+   def self.delete_all()
+     sql = "DELETE FROM match"
+     SqlRunner.run( sql )
+   end
+
 end
